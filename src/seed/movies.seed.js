57062c0ac4a847541a027f380a7bf24f864cae6a
Alba Mozas <alba.mozas@bootcamp-upgrade.com>
2023-01-15 19:34:28 +0100

    ejercicio movies 1 resuelto

---
+++ src/seed/movies.seed.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const MoviesI = require('../api/models/movies.model');
+
+
+const movies = [
+    {
+      title: 'The Matrix',
+      director: 'Hermanas Wachowski',
+      year: 1999,
+      genre: 'Acción',
+    },
+    {
+      title: 'The Matrix Reloaded',
+      director: 'Hermanas Wachowski',
+      year: 2003,
+      genre: 'Acción',
+    },
+    {
+      title: 'Buscando a Nemo',
+      director: 'Andrew Stanton',
+      year: 2003,
+      genre: 'Animación',
+    },
+    {
+      title: 'Buscando a Dory',
+      director: 'Andrew Stanton',
+      year: 2016,
+      genre: 'Animación',
+    },
+    {
+      title: 'Interestelar',
+      director: 'Christopher Nolan',
+      year: 2014,
+      genre: 'Ciencia ficción',
+    },
+    {
+      title: '50 primeras citas',
+      director: 'Peter Segal',
+      year: 2004,
+      genre: 'Comedia romántica',
+    },
+  ];
+
+  mongoose.set("strictQuery", false);
+
+  mongoose.connect('mongodb+srv://amg:root@cluster0.umzreb4.mongodb.net/moviesI?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(async () => {
+    const allMovies = await Movie.find();
+    if(allMovies.length > 0) {
+        await Movie.collection.drop();
+        console.log('delete movies');
+    }
+}).catch((error) => console.log("deleting movies", error))
+.then(async () => {
+    const moviesMap = movies.map((movie) => new Movie(movie));
+    await Movie.insertMany(moviesMap);
+    console.log("inserted movies")
+})
+.catch((error) => console.log("grong insert movies", error))
+.finally(() => mongoose.disconnect());
